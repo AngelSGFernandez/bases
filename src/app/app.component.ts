cc5986ccecf1a00ee9050d8f120999575715a09d
AngelSGFernandez <angel.fdz@mail.com>
2022-03-13 03:38:06 -0600

    The method called accumulate() was added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title: string = 'Counter App';
+  public number:number = 5;
+
+  public accumulate( value: number ) {
+    this.number += value;
+  }
 }
